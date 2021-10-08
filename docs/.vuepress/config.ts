5942298abd6f40647f57e7f21317b10702b9ecfc
田月荣 <tianyuerong@vchangyi.com>
2021-10-08 17:34:16 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,45 @@
+import { defineUserConfig } from 'vuepress-vite'
+import type { DefaultThemeOptions } from 'vuepress-vite'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-cmn-Hans',
+  title: 'vue鹅卵石',
+  description: 'Just playing around',
+
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      // NavbarGroup
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
+    sidebar: [
+      // SidebarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+        children: [
+          // SidebarItem
+          {
+            text: 'github',
+            link: 'https://github.com',
+            children: [],
+          },
+          // 字符串 - 页面文件路径
+          '/foo/bar.md',
+        ],
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
+  },
+})
---
+++ docs/README.md
@@ -0,0 +1,29 @@
+<!-- [[toc]] -->
+# 这是一个计划中的vue功能组件库
+# asdf
+Markdown 中允许直接使用 Vue 模板语法
+已废弃的 HTML 标签默认不允许在 VuePress 的 Markdown 中使用
+推荐你使用相对路径方式来引用图片
+
+## 目前计划的组件：
+- vue地图位置标注组件（基于高德api）
+- 将采用vue3 + vite + vuepress-vite 搭建
+
+
+<!-- 相对路径 -->
+[首页](../README.md)  
+[配置参考](../reference/config.md)  
+[快速上手](./getting-started.md)  
+<!-- 绝对路径 -->
+[指南](/zh/guide/README.md)  
+[配置参考 > markdown.links](/zh/reference/config.md#links)  
+<!-- URL -->
+[GitHub](https://github.com) 
+``` html
+<button>我是一个按钮</button>
+```
+这是默认主题内置的 `<Badge />` 组件 <Badge text="演示" />
+<ClientOnly>
+  <NonSsrFriendlyComponent />
+</ClientOnly>
+
